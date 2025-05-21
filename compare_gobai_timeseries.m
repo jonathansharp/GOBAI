bd4b43ad68118a84f2447b03d8da007f2787e87e
Jonathan Sharp <jonathan.sharp@noaa.gov>
2025-05-21 13:21:00 -0700

    Update timeseries script and weights function.

--- compare_gobai_timeseries.m
+++ compare_gobai_timeseries.m
@@ -103,13 +103,34 @@ global_mean = sum(reshape(GOBAI.oxy,[length(GOBAI.lon)*...
     GOBAI.vol(:),'omitnan')./(sum(GOBAI.vol(:),'omitnan'));
 plot(double(GOBAI.time),global_mean,'LineWidth',2);
 
+%% v 3.0-FFNN
+% file information
+ver = 'v3.0'; % version
+var = 'O2'; % variable
+path = ['/raid/Data/GOBAI-' var '/' ver '/']; % file path
+% download dimensions
+GOBAI.lon = ncread([path 'GOBAI-' var '-FFNN-' ver '.nc'],'lon');
+GOBAI.lat = ncread([path 'GOBAI-' var '-FFNN-' ver '.nc'],'lat');
+GOBAI.pres = ncread([path 'GOBAI-' var '-FFNN-' ver '.nc'],'pres');
+GOBAI.time = ncread([path 'GOBAI-' var '-FFNN-' ver '.nc'],'time');
+% download oxygen
+GOBAI.oxy = ncread([path 'GOBAI-' var '-FFNN-' ver '.nc'],'oxygen');
+% calculate weights
+GOBAI.vol = weights3d(GOBAI.lon,GOBAI.lat,GOBAI.pres);
+GOBAI.vol(isnan(mean(GOBAI.oxy,4,'omitnan'))) = NaN;
+% calculate global mean
+global_mean = sum(reshape(GOBAI.oxy,[length(GOBAI.lon)*...
+    length(GOBAI.lat)*length(GOBAI.pres) length(GOBAI.time)]).*...
+    GOBAI.vol(:),'omitnan')./(sum(GOBAI.vol(:),'omitnan'));
+plot(double(GOBAI.time),global_mean,'LineWidth',2);
+
 %% figure information
 f = gcf;
 f.Position(3) = f.Position(3)*2;
 datetick('x','yyyy');
 title('Global Mean GOBAI-O_{2} Versions');
 ylabel('Weighted Average [O_{2}]');
-legend({'v1.0' 'v2.0' 'v2.1' 'v2.2' 'v3.0'},'Location','northeast');
+legend({'v1.0' 'v2.0' 'v2.1' 'v2.2' 'v3.0' 'v3.0-FFNN'},'Location','northeast');
 
 %% export figure
 exportgraphics(gcf,'/raid/Data/GOBAI-O2/global_gobai_comparison.png');
